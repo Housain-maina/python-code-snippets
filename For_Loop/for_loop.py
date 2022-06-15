059d5e85bccbb159055680c5581f346fd8bfc09c
Hussaini Maina Usman <hussainmaina27@gmail.com>
2022-06-15 16:36:45 +0100

    adds for loop guide

---
+++ For_Loop/for_loop.py
@@ -0,0 +1,21 @@
+
+# iterates over a list of names and prints each name
+names = ["Hassan", "Ali", "Ahmad", "Usman", "Muhammad", "Qasim"]
+for name in names:
+    print(name)
+
+
+# iterates over a range of numbers from 0 to 9 and prints them
+for i in range(10):
+    print(i)
+
+# iterates over a range of numbers from 1 to 100 and add each number to the sum variable
+sum = 0
+for i in range(1, 101):
+    sum += i
+print(sum)
+
+# prints "Hussaini" 10 times to the console
+# use underscore if you don't need to use the value of the iteration
+for _ in range(10):
+    print("Hussaini")
