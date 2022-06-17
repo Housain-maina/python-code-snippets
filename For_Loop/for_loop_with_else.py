9f4efda272905a40a6aa942368c462b431e22ff2
Hussaini Maina Usman <hussainmaina27@gmail.com>
2022-06-17 19:41:34 +0100

    adds for_loop_with_else.py

---
+++ For_Loop/for_loop_with_else.py
@@ -0,0 +1,8 @@
+
+# using a else statement with a for loop
+
+for i in range(2, 1):
+    print(i)
+else:
+    # this will execute if the condition for executing the for loop is false
+    print("The loop is over")
