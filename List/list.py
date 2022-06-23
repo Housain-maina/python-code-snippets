7907614aa3dad0d8598738611c7c910cd8dc770a
Hussaini Usman <maina@maina>
2022-06-23 14:11:03 +0100

    adds basic intro about list

---
+++ List/list.py
@@ -0,0 +1,12 @@
+# A list is a collection of items that can be of different data types. Its numbering starts from 0.
+numbers = [5, 6, 7, 8, 9]  # list of integers
+
+names = ["Hussaini", "Maina", "Usman", "Khalid"]  # list of strings
+
+floating_numbers = [1.23, 2.34, 3.45, 4.56]  # list of floats
+
+mixed_list = [5, "Ali", 1.23, "Mustapha"]  # list of mixed data types
+
+list_of_lists = [numbers, names, floating_numbers, mixed_list]  # list of lists
+
+print(numbers, names, floating_numbers, mixed_list, list_of_lists, sep="\n\n")
