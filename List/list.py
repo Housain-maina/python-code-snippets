cc7abc7aa26cfdc7e159f5a450e6fa2d6dafd770
Hussaini Usman <maina@maina>
2022-06-23 14:31:07 +0100

    adds guide for finding length of a list

--- List/list.py
+++ List/list.py
@@ -10,3 +10,6 @@ mixed_list = [5, "Ali", 1.23, "Mustapha"]  # list of mixed data types
 list_of_lists = [numbers, names, floating_numbers, mixed_list]  # list of lists
 
 print(numbers, names, floating_numbers, mixed_list, list_of_lists, sep="\n\n")
+
+# The length of a list is the sum of the total number of items in the list
+print(len(numbers))  # prints 5 since there are 5 items in the list
