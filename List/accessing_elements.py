3be871a4a244c92649986cee97229e0eb1a5e34b
Hussaini Usman <maina@maina>
2022-06-23 14:15:45 +0100

    adds file that shows how to access elements in a list

---
+++ List/accessing_elements.py
@@ -0,0 +1,14 @@
+numbers = [5, 6, 7, 8, 9]  # list of integers
+
+# Accessing elements in a list
+print(numbers[0])  # accessing the first element in the list
+
+print(numbers[-1])  # accessing the last element in the list
+
+print(numbers[2:])  # accessing the elements from index 2 to the end of the list
+
+print(numbers[:3])  # accessing the elements from the beginning to index 3
+
+print(numbers[1:4])  # accessing the elements from index 1 to index 4
+
+print(numbers.index(7))  # finding the index of an element in the list
