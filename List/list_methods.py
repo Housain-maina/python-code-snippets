83007da7d078dce912e2e5d110a9745bcb153666
Hussaini Usman <maina@maina>
2022-06-24 15:40:22 +0100

    adds append and insert list methods

---
+++ List/list_methods.py
@@ -0,0 +1,12 @@
+names = ["Hassan", "Umar", "Abbas", "Muhammad"]
+print(names, end="\n\n")
+
+# The append method adds an item to the end of a list.
+# It takes one argument, which is the item to be added.
+names.append("Abubakar")
+print(names, end="\n\n")
+
+# The insert method adds an item to a specific index of a list.
+# It takes two arguments, the index to insert the item at and the item to be inserted.
+names.insert(2, "Jabir")
+print(names, end="\n\n")
