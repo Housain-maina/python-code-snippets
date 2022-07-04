338873ff826578933f7a7a1d203ca21c856996cf
Hussaini Usman <maina@mx>
2022-07-04 11:35:29 +0100

    add how to sort a list

--- List/list_methods.py
+++ List/list_methods.py
@@ -10,3 +10,11 @@ print(names, end="\n\n")
 # It takes two arguments, the index to insert the item at and the item to be inserted.
 names.insert(2, "Jabir")
 print(names, end="\n\n")
+
+
+numbers = [5, 3, 2, 8, 11]
+numbers.sort()
+print("sorted numbers: ", numbers)  # outputs [2, 3, 5, 8, 11]
+
+numbers.sort(reverse=True)
+print("reversed sorted numbers: ", numbers)  # outputs [11, 8, 5, 3, 2]
