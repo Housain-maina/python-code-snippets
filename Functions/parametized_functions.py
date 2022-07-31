f98a81754d81fa9b82a551c035a15fcc0720a935
Hussaini Usman <maina@mx>
2022-07-31 14:33:24 +0100

    create guide for parametized functions

---
+++ Functions/parametized_functions.py
@@ -0,0 +1,13 @@
+def welcome_user(username):  # with 1 parameter
+    print("Hello", username)
+
+
+welcome_user("Hussaini")
+welcome_user("Sadiq")
+
+
+def sum_numbers(num_1, num_2, num_3):  # with multiple parameters
+    print(num_1 + num_2 + num_3)
+
+
+sum_numbers(10, 20, 30.5)
