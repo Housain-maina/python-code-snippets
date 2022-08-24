5f84703f1fca669801fc25d502d4e3ef34cb4ac4
Hussaini Usman <maina@mx>
2022-08-24 12:22:32 +0100

    add guide to tuples

---
+++ Tuples/tuples.py
@@ -0,0 +1,13 @@
+# A tuple is an immutable data type similar to a list.
+# A tuple is also a sequence, which means it can be indexed and sliced.
+# A tuple is also an iterable, which means it can be used in a for loop.
+# A tuple is also a container, which means it can hold other tuples.
+
+parents_1 = ("Salim", "Zainab")
+print(parents_1)
+
+parents_2 = "Khalid", "Hassana"
+print(parents_2)
+
+print(len(parents_1))
+print(parents_1[0])
