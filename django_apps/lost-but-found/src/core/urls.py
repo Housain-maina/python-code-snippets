0d326550cb28eabdae238ae0840f087d8673bae9
Hussaini Usman <maina@mx>
2022-08-31 06:04:50 +0100

    configure drf_yasg documentation

--- django_apps/lost-but-found/src/core/urls.py
+++ django_apps/lost-but-found/src/core/urls.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Lost But Found API",
+        default_version="v1",
+        description="This is the Lost But Found API",
+        contact=openapi.Contact(email="admin@hussainiusman.tech"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+    authentication_classes=(JWTAuthentication,),
+)
 
 
 admin.site.site_title = "LOST BUT FOUND"
@@ -12,4 +29,7 @@ urlpatterns = [
     path("auth/", include("djoser.urls.jwt")),
     path("auth/", include("djoser.social.urls")),
     path("profiles/", include("profiles.urls")),
+    re_path(r"^(?P<format>\.json|\.yaml)$", schema_view.without_ui(cache_timeout=0), name="schema-json"),
+    path("", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
